74c67917142c6e48a66bd7972c5f0ee91f5c6251
devrdias <devrdias@gmail.com>
2019-05-20 11:21:28 -0400

    change readme

--- .history/components/CustomTextInput_20190520112039.js
+++ .history/components/CustomTextInput_20190520112039.js
@@ -1,7 +1,6 @@
 import { Input } from 'native-base';
 
 const CustomTextInput = ({ value, type, onChange, name, ...props }) => {
-	debugger;
 	return <Input {...props} value={value} onChange={event => onChange(name, type, value)} />;
 };
 
